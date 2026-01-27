e5097906c488cc8955618844a9206842759ebe23
idaemrot <mdaemrot.md@gmail.com>
2026-01-27 20:11:08 +0530

    render url updation

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL || 'http://localhost:5000/api',
+  baseURL: import.meta.env.VITE_BACKEND_URL || 'https://pizza-maniac-f9ky.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },

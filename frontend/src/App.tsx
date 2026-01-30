59f7ab2e7c652dc5647461440021a69ad4a66c37
idaemrot <mdaemrot.md@gmail.com>
2026-01-31 03:11:22 +0530

    react-snowfall tested

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,23 @@
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import { AuthProvider } from "./context/AuthContext"; // New import
-import LoginPage from "./pages/LoginPage"; // New import
-import RegisterPage from "./pages/RegisterPage"; // New import
-import ProtectedRoute from "./components/ProtectedRoute"; // New import
-import UserDashboardLayout from "./components/UserDashboardLayout"; // New import
-import AdminDashboardLayout from "./components/AdminDashboardLayout"; // New import
-import MenuPage from "./pages/User/MenuPage"; // New import
-import CartPage from "./pages/User/CartPage"; // New import
-import MyOrdersPage from "./pages/User/MyOrdersPage"; // New import
-import ProductManagerPage from "./pages/Admin/ProductManagerPage"; // New import
-import OrderManagerPage from "./pages/Admin/OrderManagerPage"; // New import
-import AdminDashboardPage from "./pages/Admin/DashboardPage"; // New import
+import { AuthProvider } from "./context/AuthContext";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import ProtectedRoute from "./components/ProtectedRoute";
+import UserDashboardLayout from "./components/UserDashboardLayout";
+import AdminDashboardLayout from "./components/AdminDashboardLayout";
+import MenuPage from "./pages/User/MenuPage";
+import CartPage from "./pages/User/CartPage";
+import MyOrdersPage from "./pages/User/MyOrdersPage";
+import ProductManagerPage from "./pages/Admin/ProductManagerPage";
+import OrderManagerPage from "./pages/Admin/OrderManagerPage";
+import AdminDashboardPage from "./pages/Admin/DashboardPage";
+
+import Snowfall from "react-snowfall";
 
 const queryClient = new QueryClient();
 
@@ -23,30 +26,60 @@ const App = () => (
     <TooltipProvider>
       <BrowserRouter>
         <AuthProvider>
-          <Routes>
-            <Route path="/" element={<Index />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/register" element={<RegisterPage />} />
+          {/* Global snow layer (does NOT affect layout) */}
+          <Snowfall
+          snowflakeCount={200}
+          style={{
+            position: "fixed",
+            top: 0,
+            left: 0,
+            width: "100vw",
+            height: "100vh",
+            pointerEvents: "none",
+            zIndex: 9999,
+          }}
+        />
 
-            {/* User Routes */}
-            <Route path="/user" element={<ProtectedRoute roles={["User"]}><UserDashboardLayout /></ProtectedRoute>}>
-              <Route index element={<MenuPage />} /> {/* Default user page */}
-              <Route path="menu" element={<MenuPage />} />
-              <Route path="cart" element={<CartPage />} />
-              <Route path="orders" element={<MyOrdersPage />} />
-            </Route>
+          {/* App content above snow */}
+          <div style={{ position: "relative", zIndex: 1 }}>
+            <Routes>
+              <Route path="/" element={<Index />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/register" element={<RegisterPage />} />
 
-            {/* Admin Routes */}
-            <Route path="/admin" element={<ProtectedRoute roles={["Admin"]}><AdminDashboardLayout /></ProtectedRoute>}>
-              <Route index element={<AdminDashboardPage />} /> {/* Default admin page */}
-              <Route path="dashboard" element={<AdminDashboardPage />} />
-              <Route path="products" element={<ProductManagerPage />} />
-              <Route path="orders" element={<OrderManagerPage />} />
-            </Route>
+              {/* User Routes */}
+              <Route
+                path="/user"
+                element={
+                  <ProtectedRoute roles={["User"]}>
+                    <UserDashboardLayout />
+                  </ProtectedRoute>
+                }
+              >
+                <Route index element={<MenuPage />} />
+                <Route path="menu" element={<MenuPage />} />
+                <Route path="cart" element={<CartPage />} />
+                <Route path="orders" element={<MyOrdersPage />} />
+              </Route>
 
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-            <Route path="*" element={<NotFound />} />
-          </Routes>
+              {/* Admin Routes */}
+              <Route
+                path="/admin"
+                element={
+                  <ProtectedRoute roles={["Admin"]}>
+                    <AdminDashboardLayout />
+                  </ProtectedRoute>
+                }
+              >
+                <Route index element={<AdminDashboardPage />} />
+                <Route path="dashboard" element={<AdminDashboardPage />} />
+                <Route path="products" element={<ProductManagerPage />} />
+                <Route path="orders" element={<OrderManagerPage />} />
+              </Route>
+
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </div>
         </AuthProvider>
       </BrowserRouter>
     </TooltipProvider>
